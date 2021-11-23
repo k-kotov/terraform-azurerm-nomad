e3c86c06e2f06dcc594b5f54d1e57bd15e0657ad
Nivall <38981440+k-kotov@users.noreply.github.com>
2021-11-23 17:33:31 +0200

    Update main.tf

--- modules/nomad-cluster/main.tf
+++ modules/nomad-cluster/main.tf
@@ -115,7 +115,7 @@ resource "azurerm_virtual_machine_scale_set" "nomad_with_load_balancer" {
   count = "${var.associate_public_ip_address_load_balancer ? 1 : 0}"
   name = "${var.cluster_name}"
   location = "${var.location}"
-  resource_group_name = "${var.resource_group_name}"
+  resource_group_name = "kostya"
   upgrade_policy_mode = "Manual"
 
   sku {
