50afe4f641e5e49d9348e51fedbdd04284c1d14e
Nivall <38981440+k-kotov@users.noreply.github.com>
2021-11-23 16:01:13 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ resource "azurerm_subnet" "nomad" {
 # ---------------------------------------------------------------------------------------------------------------------
 
 module "servers" {
-  source = "./modules/consul-cluster"
+  source = "./modules/nomand-cluster"
 
   cluster_name = "${var.cluster_name}-server"
   cluster_size = "${var.num_servers}"
