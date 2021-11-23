6b82217a15c739da06bdc6f5ce69b810901f91af
Nivall <38981440+k-kotov@users.noreply.github.com>
2021-11-23 17:24:13 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -57,7 +57,7 @@ module "servers" {
   storage_account_name = "${var.storage_account_name}"
 
   location = "${var.location}"
-  custom_data = "${data.template_file.custom_data_server.rendered}"
+  #custom_data = "${data.template_file.custom_data_server.rendered}"
   instance_size = "${var.instance_size}"
   image_id = "${var.image_uri}"
   subnet_id = "${azurerm_subnet.nomad.id}"
@@ -137,7 +137,7 @@ module "clients" {
   storage_account_name = "${var.storage_account_name}"
 
   location = "${var.location}"
-  custom_data = "${data.template_file.custom_data_client.rendered}"
+  #custom_data = "${data.template_file.custom_data_client.rendered}"
   instance_size = "${var.instance_size}"
   image_id = "${var.image_uri}"
   subnet_id = "${azurerm_subnet.nomad.id}"
